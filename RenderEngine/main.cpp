14ad45a8672d38a0923966ad1eafcfe5e64631e7
ben-rasooli <ben.rasooli@gmail.com>
2020-02-05 12:16:11 +1100

    Add getting started with triangle

--- RenderEngine/main.cpp
+++ RenderEngine/main.cpp
@@ -1,14 +1,54 @@
 #include "glm.hpp"
+#include "ext.hpp"
 #include "gl_core_4_5.h"
 #include "glfw3.h"
 
+#include <stdlib.h>
+
+//static const char* vertex_shader_text =
+//"#version 110\n"
+//"uniform mat4 MVP;\n"
+//"attribute vec3 vCol;\n"
+//"attribute vec2 vPos;\n"
+//"varying vec3 color;\n"
+//"void main()\n"
+//"{\n"
+//"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+//"    color = vCol;\n"
+//"}\n";
+//
+//static const char* fragment_shader_text =
+//"#version 110\n"
+//"varying vec3 color;\n"
+//"void main()\n"
+//"{\n"
+//"    gl_FragColor = vec4(color, 1.0);\n"
+//"}\n";
+
+//static const struct
+//{
+//	float x, y;
+//	float r, g, b;
+//} vertices[3] =
+//{
+//	{ -0.6f, -0.4f, 1.f, 0.f, 0.f },
+//	{  0.6f, -0.4f, 0.f, 1.f, 0.f },
+//	{   0.f,  0.6f, 0.f, 0.f, 1.f }
+//};
+
+GLfloat vertices[] =
+{
+	0.0f, 1.0f,
+	-1.0f, -1.0f,
+	1.0f, -1.0f
+};
+
 int main() 
 {
-	
-	if (glfwInit() == false)
+	if (!glfwInit())
 		return -1;
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Behnam Render Engine", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Render Engine", nullptr, nullptr);
 
 	if (window == nullptr)
 	{
@@ -25,18 +65,69 @@ int main()
 		return -3;
 	}
 
-	auto major = ogl_GetMajorVersion();
-	auto minor = ogl_GetMinorVersion();
-	printf("GL: %i.%i", major, minor);
+	GLuint VB_ID = 0;
+	glGenBuffers(1, &VB_ID);
+	glBindBuffer(GL_ARRAY_BUFFER, VB_ID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
+/*
+	GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+
+	glGenBuffers(1, &vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+	glCompileShader(vertex_shader);
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+	glCompileShader(fragment_shader);
+
+	program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	GLint mvp_location, vpos_location, vcol_location;
+
+	mvp_location = glGetUniformLocation(program, "MVP");
+	vpos_location = glGetAttribLocation(program, "vPos");
+	vcol_location = glGetAttribLocation(program, "vCol");
+
+	glEnableVertexAttribArray(vpos_location);
+	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+		sizeof(vertices[0]), (void*)0);
+	glEnableVertexAttribArray(vcol_location);
+	glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+		sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+
+	glm::mat4 view = glm::lookAt(glm::vec3(10, 10, 10), glm::vec3(0), glm::vec3(0, 1, 0));
+	glm::mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
+*/
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-		glfwWindowShouldClose(window) == 0)
+		!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.9f, 0.9f, 0.9f, 1);
-		glEnable(GL_DEPTH_TEST);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//float ratio;
+		//int width, height;
+		//glm::mat4 pv, mvp;
 
+		//pv = projection * view;
 
+		//glfwGetFramebufferSize(window, &width, &height);
+		//ratio = width / (float)height;
+
+		//glViewport(0, 0, width, height);
+		//glClear(GL_COLOR_BUFFER_BIT);
+
+		//glUseProgram(program);
+		//glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(pv));
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
